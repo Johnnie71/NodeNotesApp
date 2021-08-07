5150858e5ee1667ec921e479acc0db219d9f9842
johnnie71 <johnnie.agonz71@gmail.com>
2021-08-07 19:35:31 -0400

    saving notes

--- notes.js
+++ notes.js
@@ -1,13 +1,34 @@
+const fs = require('fs');
+
 function getNotes() {
     return 'Your notes...'
 }
 
 const AddNote = (title, body) => {
     const notes = LoadNotes()
+
+    notes.push({
+        title: title,
+        body: body
+    })
+
+    SaveNotes(notes)
+}
+
+const SaveNotes = (notes) => {
+    const dataJSON = JSON.stringify(notes)
+    fs.writeFileSync('notes.json', dataJSON)
 }
 
 const LoadNotes = () => {
-
+    try {
+        const dataBuffer = fs.readFileSync('notes.json');
+        const dataJSON = dataBuffer.toString();
+        return JSON.parse(dataJSON)
+    } catch (e) {
+        return []
+    }
+    
 }
 
 module.exports = {
