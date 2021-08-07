808d0300390232fc05f9d900225cb41dc2e4383c
johnnie71 <johnnie.agonz71@gmail.com>
2021-08-07 18:57:06 -0400

    added title and body

--- app.js
+++ app.js
@@ -9,8 +9,21 @@ yargs.version('1.1.0');
 yargs.command({
     command: 'add',
     describe: 'Add a new note',
-    handler: function () {
-        console.log("Adding a note!")
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note details',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        console.log('Title: ' + argv.title)
+        console.log('Body: ' + argv.body)
     }
 })
 
