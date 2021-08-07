b9bcd63883e043c8be46e35b61b3e648a77e371d
johnnie71 <johnnie.agonz71@gmail.com>
2021-08-07 18:34:50 -0400

    created more commands for yarg

--- app.js
+++ app.js
@@ -14,6 +14,33 @@ yargs.command({
     }
 })
 
+//create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function () {
+        console.log('removing note')
+    }
+})
+
+//Create a list command
+yargs.command({
+    command: 'list',
+    describe: 'List of notes',
+    handler: function () {
+        console.log("Displaying list")
+    }
+})
+
+//Create a read command
+yargs.command({
+    command: 'read',
+    describe: 'Reading note',
+    handler: function () {
+        console.log('Reading')
+    }
+})
+
 //add, remove, read, list
 
 
