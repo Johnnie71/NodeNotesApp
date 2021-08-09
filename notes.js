2382c948a437b12d79c4014d693127c44333b2fd
johnnie71 <johnnie.agonz71@gmail.com>
2021-08-08 20:55:42 -0400

    exporting list command

--- notes.js
+++ notes.js
@@ -36,7 +36,7 @@ const RemoveNotes = (title) => {
     SaveNotes(newList)
 }
 const ListNotes = () => {
-    
+
 }
 
 const SaveNotes = (notes) => {
@@ -58,5 +58,6 @@ const LoadNotes = () => {
 module.exports = {
     getNotes: GetNotes,
     addNote: AddNote,
-    removeNotes: RemoveNotes
+    removeNotes: RemoveNotes,
+    listNotes: ListNotes
 };
